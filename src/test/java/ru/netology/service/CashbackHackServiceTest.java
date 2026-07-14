cf46a1a05d4b6c940a059e7d4e493e2b5d30dc16
Foxkid <alserkol90@gmail.com>
2026-07-14 23:00:18 +0300

    Tested the service: JUnit5

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,52 @@
+package ru.netology.service;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CashbackHackServiceTest {
+
+    // Сумма меньше порогового значения
+    @Test
+    public void shouldCalculateRemainWithAmountLessBoundary() {
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 1;
+        int actual = service.remain(999);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    // Сумма выше порогового значения: не кратная 1000
+    @Test
+    public void shouldCalculateRemainWithAmountMoreBoundaryAndNotMultipleThousand() {
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 999;
+        int actual = service.remain(1001);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    // Сумма выше порогового значения: кратная 1000
+    @Test
+    public void shouldCalculateRemainWithAmountMoreBoundaryAndMultipleThousand() {
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 1000;
+        int actual = service.remain(5000);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    // Сумма равна пороговому значению
+    @Test
+    public void shouldCalculateRemainWithAmountEqualsBoundary() {
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 1000;
+        int actual = service.remain(1000);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+}
